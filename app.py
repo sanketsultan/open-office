32ce997e7215499e4d400d4f36d0f9cdc8736cb4
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-23 12:18:03 +0000

    adding Dockerfile

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def login():
-    return render_template("index.html")
+# @app.route("/login")
+# def login():
+#     return render_template("index.html")
 
-@app.route("/bookings")
-def bookings():
-    return render_template("index.html")
+# @app.route("/bookings")
+# def bookings():
+#     return render_template("index.html")
 
 
 if __name__ == '__main__':
