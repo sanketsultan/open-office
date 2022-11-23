6548564442ddddc7daa10003111e546745ddb01e
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-23 15:45:29 +0000

    Update main.tf
    adding key pair

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  key_name = "terraform"
   user_data	= file("linux.sh")
 
   tags = {
