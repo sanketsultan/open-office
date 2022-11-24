ca7bf0afbdc84e211aa9060d89ad16acc0bbe46d
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-24 00:49:05 +0000

    create ec2

--- main.tf
+++ main.tf
@@ -12,44 +12,49 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
-data "aws_security_group" "selected" {
-  name = "launch-wizard-13"
-}
 
-resource "aws_security_group" "allow_ssh" {
+# ###########Security Group#####################
 
-  description = "Allow ssh inbound traffic"
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = [aws_vpc.main.cidr_block]
+#     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
+
+
 resource "aws_instance" "app_server" {
   ami = "ami-830c94e3"
   instance_type = "t2.micro"
   key_name = "terraform"
   user_data	= file("linux.sh")
-  vpc_security_group_ids = [
-    "launch-wizard-13"
-  ]
   tags = {
     Name = "open-office"
   }
 }
 
-variable "docker-image" {
-  type        = string
-  description = "name of the docker image to deploy"
-  default     = "vison91/office_spaces"
-}
+# variable "docker-image" {
+#   type        = string
+#   description = "name of the docker image to deploy"
+#   default     = "vison91/office_spaces"
+# }
