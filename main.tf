befd25b60f9febfd471038a75b0810d442a845d7
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-24 23:38:24 +0000

    updated JF hardcoded action

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_security_group" "devops-project" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
   ingress {
     description      = "HTTPS"
     from_port        = 443
@@ -42,6 +43,7 @@ resource "aws_security_group" "devops-project" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  
   ingress {
     description      = "HTTP"
     from_port        = 80
