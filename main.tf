f8a16e0858b1eb9e38cfb8a6ca4f67fc96e514fd
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-24 01:28:36 +0000

    key-pair

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "devops-project" {
 resource "aws_instance" "app_server" {
   ami = "ami-096800910c1b781ba"
   instance_type = "t3.medium"
-  key_name = "terraform"
+  key_name = "terraform-key"
   vpc_security_group_ids = ["${aws_security_group.devops-project.name}"]
   user_data	= file("linux.sh")
   tags = {
