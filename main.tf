1967be855fefb89634990a6549d9c9f0037fa90a
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-25 01:07:05 +0000

    d

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_instance" "app_server" {
     type = "ssh"
     user        = "ubuntu"
     host = "${self.private_ip}"
-    private_key = "${file("/home/ubuntu/terraform-key.pem")}"
+    private_key = "file(/home/ubuntu/terraform-key.pem)"
     agent = false
   } 
 
