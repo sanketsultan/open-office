0d595129a23f73a841a3a5f7763cbdd8628b70d5
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-26 17:34:28 +0000

    updated project

--- main.tf
+++ main.tf
@@ -44,6 +44,14 @@ resource "aws_security_group" "devops-project" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    description      = "HTTP"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
