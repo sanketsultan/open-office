110820e118430a438c80b30833f97b5f8324d662
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-26 16:16:51 +0000

    updated project

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 touch sanket
+touch ravi
 sudo apt update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
