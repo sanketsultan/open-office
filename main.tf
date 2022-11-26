9e0553503b128477c071fe5503d2c9aff707138f
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-26 15:52:11 +0000

    updated project

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_instance" "app_server" {
     host        = self.public_ip
     private_key = file("terraform-key.pem")
     # public_key = file("/home/ubuntu/terraform-key.pem")
-    #agent = true
+    agent = true
   }
   provisioner "remote-exec" {
     inline = [
