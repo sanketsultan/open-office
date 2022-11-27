a57a726431e0fc7d4dbd3ea74dc13779d4b42bd6
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-27 01:38:22 +0000

    updated project

--- aws-linux.sh
+++ aws-linux.sh
@@ -2,8 +2,10 @@
 sudo yum install git -y
 mkdir project
 cd project/
+chmod 777 project
 git clone https://github.com/sanketsultan/open-office.git
 cd open-office/
+chmod 777 open-office
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
