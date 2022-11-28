d0745566c864152d2483de98110ca5a2be2891b7
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-28 17:33:50 +0000

    s

--- aws-linux.sh
+++ aws-linux.sh
@@ -6,5 +6,5 @@ cd open-office/
 sudo python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-# setsid python3 app.py &
+./open-office/run.sh
 echo "file executed successfully"
