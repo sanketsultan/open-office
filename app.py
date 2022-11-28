2fe779fc75e6f28e1a272c98aa894f5fa50dcdf6
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-28 15:46:33 +0000

    d

--- app.py
+++ app.py
@@ -46,8 +46,6 @@ def bookingsInfoUpdate(bookingInfo):
     bookingdb.updaterecords(customerData)
     return('/')
 
-
-
 if __name__ == "__main__":
     # port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
