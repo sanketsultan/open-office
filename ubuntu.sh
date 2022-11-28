aae855da45d432f02874ac2a0c55e88fdd6d2581
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-28 18:52:28 +0000

    s

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,9 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt install python-is-python3 -y
 sudo DEBIAN_FRONTEND=noninteractive apt install python3-virtualenv -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install pip -y
-cd /home/ubuntu/open-office/
+sudo apt-get install git -y
+git clone https://github.com/sanketsultan/open-office.git
+cd open-office/
 virtualenv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
