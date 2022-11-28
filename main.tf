7b90719382b91a01c29217b801b8d8bfbfe61b0f
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-11-28 16:57:48 +0000

    s

--- main.tf
+++ main.tf
@@ -60,14 +60,6 @@ resource "aws_security_group" "devops-project" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  ingress {
-    description      = "HTTP"
-    from_port        = 5000
-    to_port          = 5000
-    protocol         = "tcp"
-    cidr_blocks      = ["127.0.0.1/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
   egress {
     from_port        = 0
     to_port          = 0
