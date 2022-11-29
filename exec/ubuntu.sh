c731c3c498f11b088dcf45616325697cf373c1a8
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2022-11-29 17:03:55 +0000

    updated shell script

--- exec/ubuntu.sh
+++ exec/ubuntu.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install python-is-python3 -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install python3-virtualenv -y
