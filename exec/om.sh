aaebf8fe98c47131a3753ed3337a6f13fb6b077c
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2022-12-15 01:37:33 +0000

    Update om.sh

--- exec/om.sh
+++ exec/om.sh
@@ -1,1 +1,22 @@
 #! /bin/bash
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install python-is-python3 -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install python3-virtualenv -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-pip -y
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+sudo apt-get install git -y
+sudo apt install python3-venv -y
+cd /ML_Deployment/ML\ CA1/
+python3 -m venv ML\ CA1/
+source ML\ CA1/bin/activate
+pip install wheel
+pip install uwsgi flask
+sudo ufw allow 5000
+pip3 install numpy
+pip3 install pandas
+pip3 install -U scikit-learn
+python3 app.py
